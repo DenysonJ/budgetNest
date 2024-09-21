8c3d545b187a41885d6218c1bf4063f82890bd6e
Denyson Grellert <denysonjurgen@gmail.com>
2024-09-21 20:29:10 -0300

    Initial commit

---
+++ database/migrations/20240921161224_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
+    created_at datetime(3) NOT NULL,
+    updated_at datetime(3) NOT NULL,
+    PRIMARY KEY (id),
+    KEY idx_users_created_at (created_at),
+    KEY idx_users_updated_at (updated_at),
+    KEY idx_users_email (email)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
